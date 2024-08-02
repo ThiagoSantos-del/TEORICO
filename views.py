51786eb65b4b76c5d89739c0340b098457c62bd6
Thiago Ferreira <thiago51295486@aluno.ms.senac.br>
2024-08-01 21:25:18 -0400

    adicionando arquivo para views.py

---
+++ views.py
@@ -0,0 +1,15 @@
+import classe
+
+def menu_principal ():
+
+    print('1 - Criar cao')
+    print('2 - Criar humano')
+    print('3 - Listar cao')
+    print('4 - Listar humano')
+    print('5 - Excluir cao')
+    print('6 - Excluir humano')
+    print('7 - Editar cao')
+    print('8 - Editar humano')
+    print('9 - Adicionar cao')
+    print('10 - adicionar humano')
+
